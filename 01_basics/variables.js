02d53c0293707a099acc23f080e63571b50c31b1
khanajmal8055 <154616823+khanajmal8055@users.noreply.github.com>
2025-06-23 17:22:49 +0000

    container config done

---
+++ 01_basics/variables.js
@@ -0,0 +1,21 @@
+const accountId = 12345
+let accountEmail = "ak123@gmail.com"
+var accountPassword = "456"
+accountCity = "bokaro"
+let accountState
+
+/*
+if declare variable with const the we cannot re-intailize it , if we do so then it will throw
+an error called assignment to contant variables 
+*/
+// accountId= 4321
+
+
+// console.log(accountId);
+
+accountEmail = "aj786@gmal.com"
+accountPassword = "123"
+accountCity = "chas"
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
