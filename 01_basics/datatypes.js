3773be06830989c079558b48ec28dc849bb97226
khanajmal8055 <154616823+khanajmal8055@users.noreply.github.com>
2025-06-23 17:49:08 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,16 @@
+"use strict" // treat all js code as newer version
+
+let name = "Ajmal"
+let age = 18
+let isLoggedIn = true
+
+console.log(typeof name);
+
+
+// datatypes
+// number => 2*52
+// string
+// boolean
+// null
+// symbol
+// undefined
