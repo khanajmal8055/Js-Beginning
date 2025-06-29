a12e068ebcfb6ade9b9d50874d52456f3eb43ea8
khanajmal8055 <154616823+khanajmal8055@users.noreply.github.com>
2025-06-29 08:29:31 +0000

    number and maths in javascript

---
+++ 01_basics/num_and_maths.js
@@ -0,0 +1,24 @@
+const score = 400 
+// console.log(score);
+
+const balance = new Number (100.00)
+// console.log(balance);
+
+const fixed = balance.toFixed(1)
+// console.log(fixed);
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Maths in javascript
+
+
+console.log(Math.random());
+
+console.log((Math.random()*10) +1);
+
+console.log(Math.floor((Math.random()*10) +1))
+
+const min = 10
+const max = 20
+
+console.log(Math.floor((Math.random() * (max - min + 1)) + min));
