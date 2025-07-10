82b2b0488b5bf8c0a3d9059ae3016fdd12d433dc
khanajmal8055 <154616823+khanajmal8055@users.noreply.github.com>
2025-07-10 16:50:25 +0000

    Function

--- 03_basics/function.js
+++ 03_basics/function.js
@@ -40,3 +40,35 @@ function handleObject (getobject){
 let ans = handleObject(user)
 console.log(ans);
 
+
+// closure check scope level
+
+
+function outer(){
+    const userName = "Ajmal"
+
+    function inner (){
+        const website = " youtube"
+        console.log(userName + website); // here variable of outer function is accessibe 
+    }
+    // console.log(website); // here variable of inner cannot accessible
+    inner()
+    
+
+}
+outer()
+
+console.log(addOne(5)); // we can call addOne function here also if the function declaration is like that mention below 
+
+function addOne(num){ 
+    return num + 1
+}
+// addOne(5) // we can call addOne function here also  
+
+
+// addTwo() // we cannot call the function here
+const addTwo = function(num){
+    return num + 2
+}
+
+addTwo(7) // we can only call the function here onlu if fuction declared like the above 
